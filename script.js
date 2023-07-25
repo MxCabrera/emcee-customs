0552e01dbeda186b66bd2516d2d97f1db58db6c9
MxCabrera <migueledward@hotmail.com>
2023-07-25 15:00:12 -0400

    fixed the array logic in js. added separate script js files for each page

--- script.js
+++ script.js
@@ -1,11 +1,205 @@
 console.log('it works');
+const app = {}
 
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+// hamburger drop down menu js logic :
+app.hamburger = document.querySelector('.hamburger');
+app.navLink = document.querySelector('.nav__link');
+app.gallery = document.querySelector('.galleryContainer');
+// app.service = document.querySelector('.serviceContainer');
 
-hamburger.addEventListener('click', () => {
-   navLink.classList.toggle('hide');
+// on click that adds a hide class to element
+app.hamburger.addEventListener('click', () => {
+   app.navLink.classList.toggle('hide');
 });
+
+app.gallerys = [
+   {
+      name: 'Inuyasha x Inuyasha Demon Air Force 1 Low Top',
+      image: 'IMG_1047.jpg',
+      description: `A custom hand painted drawing of Inuysha, and the demon form of the main character of his own anime show, "Inuyasha"`
+
+   },
+   {
+      name: 'Sasuke x Itachi Uchiha Air Force 1 High Top',
+      image: 'IMG_1047.jpg',
+      description: `A custom hand painted drawing of Sasuke Uchiha and his older Brother Itachi, along with it's family inspired "Sharingan", from the classic anime show "Naruto"`
+
+   },
+   {
+      name: 'Attack x Scared - Zenitsu Air Force 1 Utility High',
+      image: 'IMG_1047.jpg',
+      description: `A custom hand painted drawing of Zenitsu, the Thunder breathing slayer in both attack mode and frightened scene from the spider demons, shown in the latest anime, "Demonslayer"`
+   },
+   {
+      name: 'Itadori Yuuji x Gojo Satoru Nike Cortez',
+      image: 'IMG_1047.jpg',
+      description: `A custom hand painted drawing of Itadori Yuuji and Sensei Gojo Satoru created using the art design from the end credits of the upcoming anime, "Jujutsu Kaisen"`
+   }
+]
+
+// app.services = [
+//    {
+//       name: 'Basic Cleaning',
+//       description: `Base cleaning session`,
+//       price: '$40',
+//    },
+//    {
+//       name: 'Deep Cleaning',
+//       description: `Deeper cleaning session, helps remove all the dirt and grime in the hard the reach areas as well as the inside & bottom sole`,
+//       price: '$55',
+//    },
+//    {
+//       name: 'Sole Icing',
+//       description: `Icing the sole of your shoes. Using a solution, it will turn your yellow'd soles back to its transparent icy blue form`,
+//       price: '$20/per session',
+//    },
+//    {
+//       name: 'Re-paint touch ups',
+//       description: `Service provided to areas of the shoe where the paint has scratched off or lost its color during use. Will go through process of initial coat removal, sanding, color-matching, painting, and acrylic finisher `,
+//       price: '$25/per shoe',
+//    },
+//    {
+//       name: 'Re-paint sections',
+//       description: `Service provided to areas of the shoe where a large section of paint has scratched off or lost its color during use. Will go through process of initial coat removal, sanding, color-matching, painting, and acrylic finisher `,
+//       price: '$45/per shoe',
+//    },
+//    {
+//       name: 'Full Color Swap Custom',
+//       description: `Changing the entire color way of the shoe to match the clients request. No designs, just color changes (Up to 4 colors max)`,
+//       price: '$160',
+//    },
+//    {
+//       name: 'Full Restoration',
+//       description: `Restoring an older or beat up pair of shoes to its almost original look. Includes deep cleaning, sole icing, repaint section, and inside cleaning`,
+//       price: 'price varies according to client needs',
+//    },
+//    {
+//       name: 'Custom Shoe Design',
+//       description: `Customizing a clients shoe based on their request/needs. Could have any character, quotes, items painted on it, as well as any color selections`,
+//       price: 'starting rate at $110 - price varies according to client needs',
+//    },
+// ];
+
+app.addListeners = () => {
+   app.galleryAppend()
+   // app.serviceAppend();
+   // if (app.gallery === null) {
+   //    console.log('gallery is null')
+   //    app.gallerys.forEach((design) => {
+   //       const firstListItem = document.createElement('li');
+   //       firstListItem.classList.add('design');
+   //       firstListItem.innerHTML = `
+   //       <div class="designPic">
+   //       <img src="${design.image}" alt="${design.name}"
+   //       </div>
+   //       <div class="designName">
+   //       <h2 class="customName">${design.name}</h2>
+   //       </div>
+   //       <div class="designDescrip">
+   //       <p>${design.description}</p>
+   //       </div>
+   //       <div class="serviceButtons">
+   //       <button>Add to cart</button>
+   //       <button>Contact Us</button>
+   //       </div>
+   //       `
+   //       app.gallery.appendChild(firstListItem);
+   
+   //    });
+   // } else if (app.service === null){
+   //    console.log('service is null');
+   //    app.services.forEach((service) => {
+   //       console.log(service)
+   //       const newListItem = document.createElement('li');
+   //       newListItem.classList.add('services');
+   //       // newListItem.setAttribute('data-aos', 'fade-up');
+   //       newListItem.innerHTML = `
+   //       <div class="serviceName">
+   //       <h2 class="customName">${service.name}</h2>
+   //       </div>
+   //       <div class="serviceDescrip">
+   //       <p>${service.description}</p>
+   //       <p> Price: ${service.price}</p>
+   //       </div>
+   //       <div class="serviceButtons">
+   //       <button>Add to cart</button>
+   //       <button>Contact Us</button>
+   //       </div>
+   //       `
+   //       app.service.appendChild(newListItem);
+   //       console.log(newListItem)
+   //    })
+   // }
+}
+
+
+// app.serviceAppend = () => {
+//    console.log(app.service);
+//    app.services.forEach((service) => {
+//       console.log(service)
+//       const newListItem = document.createElement('li');
+//       newListItem.classList.add('services');
+//       // newListItem.setAttribute('data-aos', 'fade-up');
+//       newListItem.innerHTML = `
+//       <div class="serviceName">
+//       <h2 class="customName">${service.name}</h2>
+//       </div>
+//       <div class="serviceDescrip">
+//       <p>${service.description}</p>
+//       <p> Price: ${service.price}</p>
+//       </div>
+//       <div class="serviceButtons">
+//       <button>Add to cart</button>
+//       <button>Contact Us</button>
+//       </div>
+//       `
+//       app.service.appendChild(newListItem);
+//       console.log(newListItem)
+//    })
+// }
+
+
+
+app.galleryAppend = () => {
+   app.gallerys.forEach((design) => {
+      const firstListItem = document.createElement('li');
+      firstListItem.classList.add('design');
+      firstListItem.innerHTML = `
+      <div class="designPic">
+      <img src="${design.image}" alt="${design.name}"
+      </div>
+      <div class="designName">
+      <h2 class="customName">${design.name}</h2>
+      </div>
+      <div class="designDescrip">
+      <p>${design.description}</p>
+      </div>
+      <div class="serviceButtons">
+      <button>Add to cart</button>
+      <button>Contact Us</button>
+      </div>
+      `
+      app.gallery.appendChild(firstListItem);
+      
+   });
+};
+
+app.hey = () => {
+   console.log('hey')
+}
+
+
+app.init = () => {
+   app.addListeners();
+   app.hey()
+
+
+
+   console.log('init');
+}
+
+app.init()
+
 
 
 // custom shoe business page that allows users to check out the services we provide, as well as visuals that have currently been created.
@@ -24,3 +218,5 @@ hamburger.addEventListener('click', () => {
 // search bar to check/filter through ... services?
 // paypal integration
 // 
+
+
---
+++ service.js
@@ -0,0 +1,88 @@
+const app = {}
+
+// hamburger drop down menu js logic :
+app.hamburger = document.querySelector('.hamburger');
+app.navLink = document.querySelector('.nav__link');
+
+
+app.service = document.querySelector('.serviceContainer');
+
+app.hamburger.addEventListener('click', () => {
+   app.navLink.classList.toggle('hide');
+});
+
+app.services = [
+   {
+      name: 'Basic Cleaning',
+      description: `Base cleaning session`,
+      price: '$40',
+   },
+   {
+      name: 'Deep Cleaning',
+      description: `Deeper cleaning session, helps remove all the dirt and grime in the hard the reach areas as well as the inside & bottom sole`,
+      price: '$55',
+   },
+   {
+      name: 'Sole Icing',
+      description: `Icing the sole of your shoes. Using a solution, it will turn your yellow'd soles back to its transparent icy blue form`,
+      price: '$20/per session',
+   },
+   {
+      name: 'Re-paint touch ups',
+      description: `Service provided to areas of the shoe where the paint has scratched off or lost its color during use. Will go through process of initial coat removal, sanding, color-matching, painting, and acrylic finisher `,
+      price: '$25/per shoe',
+   },
+   {
+      name: 'Re-paint sections',
+      description: `Service provided to areas of the shoe where a large section of paint has scratched off or lost its color during use. Will go through process of initial coat removal, sanding, color-matching, painting, and acrylic finisher `,
+      price: '$45/per shoe',
+   },
+   {
+      name: 'Full Color Swap Custom',
+      description: `Changing the entire color way of the shoe to match the clients request. No designs, just color changes (Up to 4 colors max)`,
+      price: '$160',
+   },
+   {
+      name: 'Full Restoration',
+      description: `Restoring an older or beat up pair of shoes to its almost original look. Includes deep cleaning, sole icing, repaint section, and inside cleaning`,
+      price: 'price varies according to client needs',
+   },
+   {
+      name: 'Custom Shoe Design',
+      description: `Customizing a clients shoe based on their request/needs. Could have any character, quotes, items painted on it, as well as any color selections`,
+      price: 'starting rate at $110 - price varies according to client needs',
+   },
+];
+
+app.addListeners = () => {
+      console.log(app.service);
+      app.services.forEach((service) => {
+         console.log(service)
+         const newListItem = document.createElement('li');
+         newListItem.classList.add('services');
+         // newListItem.setAttribute('data-aos', 'fade-up');
+         newListItem.innerHTML = `
+      <div class="serviceName">
+      <h2 class="customName">${service.name}</h2>
+      </div>
+      <div class="serviceDescrip">
+      <p>${service.description}</p>
+      <p> Price: ${service.price}</p>
+      </div>
+      <div class="serviceButtons">
+      <button>Add to cart</button>
+      <button>Contact Us</button>
+      </div>
+      `
+         app.service.appendChild(newListItem);
+         console.log(newListItem)
+      })
+}
+
+
+app.init = () => {
+   app.addListeners();
+}
+
+
+app.init()
