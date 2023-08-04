2920efce0f270d5034c5365986c02cdaf36cec57
MxCabrera <migueledward@hotmail.com>
2023-08-04 18:38:39 -0400

    fixed cart in the service page, you can now remove product in the cart, gives users msg if cart is empty

--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-const app = {}
+const app = {};
 
 // hamburger drop down menu js logic :
 app.hamburger = document.querySelector('.hamburger');
@@ -7,6 +7,7 @@ app.navLink = document.querySelector('.nav__link');
 app.modal = document.getElementById('myModal');
 app.span = document.querySelector('.close');
 app.serviceModal = document.querySelector('.serviceData');
+app.userCart = document.getElementById('userCart');
 // connecting to serviceContainer element
 app.service = document.querySelector('.serviceContainer');
 
@@ -72,20 +73,89 @@ app.services = [
       price: 'starting rate at $120.00 - price varies according to client needs',
       cost: 120,
       code: 'csd'
-   },
+   }
 ];
 
-// add listeners function that is called by app.init
-app.addListeners = () => {
-   let counter = 0; 
+app.emptyCart = () => {
+   // if service modal container is empty, tell users theres nothing in the cart
+   if (app.serviceModal.innerHTML == '') {
+      // adding content into element.
+      app.serviceModal.innerHTML = `
+         <div class="emptyCart">
+            <h4>Theres nothing in your cart</h4>
+         </div>
+      `;
+   };
+};
 
-   app.services.forEach((service) => {
-      const newListItem = document.createElement('li');
-      newListItem.classList.add('services');
-      newListItem.classList.add(`services${counter}`);
-      newListItem.setAttribute('id', `cart${counter}`)
-      // newListItem.setAttribute('data-aos', 'fade-up');
-      newListItem.innerHTML = `
+app.showCart = () => {
+   app.modal.style.display = 'block';
+}
+app.hideCart = () => {
+   app.modal.style.display = 'none';
+}
+
+app.addProduct = (cartCounter, data, newData) => {
+   if (app.serviceModal.innerHTML == '\n         <div class="emptyCart">\n            <h4>Theres nothing in your cart</h4>\n         </div>\n      ') {
+      // clear the text in the html element.
+      app.serviceModal.innerHTML = '';
+   };
+
+   // adding a new product in the user cart.
+   newData.innerHTML = `
+         <div class="product${cartCounter} product">
+            <div class="leftside">
+               <h1>- ${data.name}</h1>
+               <p>Price:$${data.cost}.00</p>
+            </div>
+            <div class="rightside">
+               <button class="remove${cartCounter}">remove</button>
+            </div>
+         </div>
+
+         `;
+   // inserting the product as a child to the parent element.
+   cartCounter++;
+   app.serviceModal.appendChild(app.newData);
+
+   // connecting the remove buttons in the cart
+   app.removeItemOne = document.querySelector('.remove1');
+   app.removeItemTwo = document.querySelector('.remove2');
+   app.removeItemThree = document.querySelector('.remove3');
+   app.removeItemFour = document.querySelector('.remove4');
+   app.removeItemFive = document.querySelector('.remove5');
+   app.removeItemSix = document.querySelector('.remove6');
+   app.removeItemSeven = document.querySelector('.remove7');
+   app.removeItemEight = document.querySelector('.remove8');
+   app.removeItemNine = document.querySelector('.remove9');
+}
+
+
+// if user clicks cart in nav bar, make modal appear in DOM
+app.userCart.addEventListener('click', ()=>{
+   // display: block;
+   app.showCart();
+   console.log(app.serviceModal);
+   app.emptyCart();
+});
+
+// if user clicks on x button in the modal, remove modal from DOM.
+app.span.addEventListener('click', () => {
+   // hiding the modal from the user
+   app.hideCart();
+});
+
+
+app.appendService = (counter, service) => {
+   // li html element creation stored in variable
+   const newListItem = document.createElement('li');
+   // adding class to html element
+   newListItem.classList.add('services');
+   newListItem.classList.add(`services${counter}`);
+   // adding an ID to the element
+   newListItem.setAttribute('id', `cart${counter}`);
+   // adding content to html element
+   newListItem.innerHTML = `
          <div class="serviceName">
             <h2 class="customName">${service.name}</h2>
          </div>
@@ -99,28 +169,45 @@ app.addListeners = () => {
             <a href="./contact.html">Contact Us</a>
             </button>
          </div>
-         `
-      app.service.appendChild(newListItem);
-      // console.log(newListItem);
+         `;
+   // inserting html element as a child to parent container
+   app.service.appendChild(newListItem);
+}
 
 
+
+// add listeners function that is called by app.init
+app.addListeners = () => {
+   // base counter for tracking
+   let counter = 0; 
+
+   // forEach method that loops through service array.
+   app.services.forEach((service) => {
+      app.appendService(counter, service);
+
+      // storing html element by ID into variable.
       app.btn = document.getElementById(`cart${counter}`);
 
+      // if user click on the button, make modal appear in DOM. 
       app.btn.addEventListener('click', () => {
-         app.modal.style.display = 'block';
+         app.showCart();
       });
 
+      // window event listener if user click on window.
       window.addEventListener('click', (e) => {
-         // console.log(e.target)
+         // we dont need this
          const hasClass = e.target.classList.contains(`customPic1`);
+         // if user clicks on app.modal, then remove the modal.
          if (e.target == app.modal) {
-            app.modal.style.display = 'none';
-         }
+            app.hideCart();
+         };
       });
-      
+      // add 1 to counter 
       counter++;
    });
    
+   
+   // connecting the add to cart button to the correct html element.
    app.contactButtonOne = document.querySelector('.contactButton0');
    app.contactButtonTwo = document.querySelector('.contactButton1');
    app.contactButtonThree = document.querySelector('.contactButton2');
@@ -130,96 +217,180 @@ app.addListeners = () => {
    app.contactButtonSeven = document.querySelector('.contactButton6');
    app.contactButtonEight = document.querySelector('.contactButton7');
 
+   // when user clicks on the service they want, append it to the DOM in the cart
    app.contactButtonOne.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[0]);
-   })
+   });
 
    app.contactButtonTwo.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[1]);
-   })
+   });
 
    app.contactButtonThree.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[2]);
-   })
+   });
 
    app.contactButtonFour.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[3]);
-   })
+   });
 
    app.contactButtonFive.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[4]);
-   })
+   });
 
    app.contactButtonSix.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[5]);
-   })
+   });
 
    app.contactButtonSeven.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[6]);
-   })
+   });
 
    app.contactButtonEight.addEventListener('click', ()=>{
-      console.log('hey you clicked it!');
       app.showDetails(app.services[7]);
-   })
+   });
 
-   app.addCart = document.querySelector('.addCart');
-   
-   app.addCart.addEventListener('click', (e)=>{
-      e.preventDefault();
-      console.log('add to cart!');
-   })
 
+   app.removeProduct = () => {
+      // connecting the productBox for eventual deletion 
+      app.deletedOne = document.querySelector('.productBox1');
+      app.deletedTwo = document.querySelector('.productBox2');
+      app.deletedThree = document.querySelector('.productBox3');
+      app.deletedFour = document.querySelector('.productBox4');
+      app.deletedFive = document.querySelector('.productBox5');
+      app.deletedSix = document.querySelector('.productBox6');
+      app.deletedSeven = document.querySelector('.productBox7');
+      app.deletedEight = document.querySelector('.productBox8');
+      app.deletedNine = document.querySelector('.productBox9');
+
+      // when user clicks, remove the box that corresponds to what user removed. 
+      app.removeItemOne.addEventListener('click', () => {
+         app.deletedOne.remove();
+         app.emptyCart();
+      });
+      app.removeItemTwo.addEventListener('click', () => {
+         app.deletedTwo.remove();
+         app.emptyCart();
+      });
+      app.removeItemThree.addEventListener('click', () => {
+         app.deletedThree.remove();
+         app.emptyCart();
+      });
+      app.removeItemFour.addEventListener('click', () => {
+         app.deletedFour.remove();
+         app.emptyCart();
+      });
+      app.removeItemFive.addEventListener('click', () => {
+         app.deletedFive.remove();
+         app.emptyCart();
+      });
+      app.removeItemSix.addEventListener('click', () => {
+         app.deletedSix.remove();
+         app.emptyCart();
+      });
+      app.removeItemSeven.addEventListener('click', () => {
+         app.deletedSeven.remove();
+         app.emptyCart();
+      });
+      app.removeItemEight.addEventListener('click', () => {
+         app.deletedEight.remove();
+         app.emptyCart();
+      });
+      app.removeItemNine.addEventListener('click', () => {
+         app.deletedNine.remove();
+         app.emptyCart();
+      });
+   }
    
    
    let cartCounter = 1;
-   app.serviceCheck = [];
+   
+   // function that will add and show product users added into cart. 
    app.showDetails = (data) => {
+      // empty array for future use
+      app.serviceCheck = [];
+      // li element creation, storing in variable
       app.newData = document.createElement('li');
+      // adding a class to every new product
+      app.newData.classList.add(`productBox${cartCounter}`);
+      // forEach method
       app.serviceCheck.forEach((item)=>{
-         console.log(item);
+         // if the serivce code matches any item in the array, let us know. 
          if (data.code === item) {
-            console.log(`yes, ${data.code} matches with item inside serviceCheck`)
-            
+            console.log(`yes, ${data.code} matches with item inside serviceCheck`);
          } else {
             console.log('no match!')
-            console.log(app.serviceCheck)
-         }
-         // if data.code === item break.
-         // else push code into data. 
-      })
-      app.newData.innerHTML = `
-         <div class="product">
-         <h1>${cartCounter}: ${data.name}</h1>
-         <p>Price:$${data.cost}.00</p>
-         </div>
-         `;
-      app.serviceCheck.push(data.code)
-      app.serviceModal.appendChild(app.newData);
+            console.log(app.serviceCheck);
+         };
+      });
+      
+      app.addProduct(cartCounter, data, app.newData)
+
+      // pushing service code into array to check is user has already added the product.
+      app.serviceCheck.push(data.code);
+
+      // adding 1 to the counter variable 
       cartCounter++;
-   }   
-   
-   app.span.addEventListener('click', () => {
-      // hiding the modal from the user
-      app.modal.style.display = 'none';
-   });
+      // remove product in cart if user clicks.
+      app.removeProduct();
+
+      // // connecting the productBox for eventual deletion 
+      // app.deletedOne = document.querySelector('.productBox1');
+      // app.deletedTwo = document.querySelector('.productBox2');
+      // app.deletedThree = document.querySelector('.productBox3');
+      // app.deletedFour = document.querySelector('.productBox4');
+      // app.deletedFive = document.querySelector('.productBox5');
+      // app.deletedSix = document.querySelector('.productBox6');
+      // app.deletedSeven = document.querySelector('.productBox7');
+      // app.deletedEight = document.querySelector('.productBox8');
+      // app.deletedNine = document.querySelector('.productBox9');
+      
+      // // when user clicks, remove the box that corresponds to what user removed. 
+      // app.removeItemOne.addEventListener('click', () => {
+      //    app.deletedOne.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemTwo.addEventListener('click', () => {
+      //    app.deletedTwo.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemThree.addEventListener('click', () => {
+      //    app.deletedThree.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemFour.addEventListener('click', () => {
+      //    app.deletedFour.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemFive.addEventListener('click', () => {
+      //    app.deletedFive.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemSix.addEventListener('click', () => {
+      //    app.deletedSix.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemSeven.addEventListener('click', () => {
+      //    app.deletedSeven.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemEight.addEventListener('click', () => {
+      //    app.deletedOne.remove();
+      //    app.emptyCart();
+      // });
+      // app.removeItemNine.addEventListener('click', () => {
+      //    app.deletedNine.remove();
+      //    app.emptyCart();
+      // });
+   };
 };
 
-
-
-
-
+// app.init function to initialize the web application.
 app.init = () => {
+   // addListeners function call
    app.addListeners();
 };
 
-
+// calling app.init function
 app.init();
 
